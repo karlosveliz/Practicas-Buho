3b260785068a5f18869cc0e215bd82a0b8c9c5a5
karlosveliz <126271356+karlosveliz@users.noreply.github.com>
2023-10-06 14:52:17 -0500

    Add files via upload

---
+++ Practica_Reu2/my-app/dist/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'), // Cambia 'dist' a 'src'
+    },
+  },
+};
